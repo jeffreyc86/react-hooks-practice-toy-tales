5526336a69f0c336ed5bcb20ce706ac3d8767214
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-07 10:33:47 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,49 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const API = "http://localhost:3001/toys"
+  const [toys, setToys] = useState([])
+
+  useEffect(()=>{
+    fetch(API)
+    .then(res=>res.json())
+    .then(toysArr => {
+      setToys(toysArr)
+    })
+  }, [])
+
+  const updateToyArr = (updatedObj) => {
+    const newArray = toys.map(toy => {
+      if (updatedObj.id === toy.id) {
+        return updatedObj
+      } else {
+        return toy
+      }
+    })
+    setToys(newArray)
+  }
+
+  function deleteToy(id){
+    const newArray = toys.filter(toy => {return toy.id != id})
+    setToys(newArray)
+  }
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function addToy(newToy) {
+    setToys([...toys, newToy])
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm addToy={addToy} /> : null}
       <div className="buttonContainer">
-        <button onClick={handleClick}>Add a Toy</button>
+        <button onClick={handleClick}>{!showForm ? "Add a Toy" : "Nevermind"}</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} updateToyArr={updateToyArr} deleteToy={deleteToy}/>
     </>
   );
 }
