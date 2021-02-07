0083751fd8774215e11478154ac6cc751242f688
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-07 10:33:43 -0500

    commit tree 7151014cdfa3ece25ae33db0919823df1e58e1a5

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({toy, updateToyArr, deleteToy}) {
+
+  const {id, name, image, likes} = toy
+
+  function handleLike() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: 'PATCH',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({likes: (likes + 1)})
+    })
+    .then(res=>res.json())
+    .then(updatedToy => {
+      updateToyArr(updatedToy)
+    })
+  }
+
+  function handleDelete(){
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: 'DELETE'
+    })
+    .then(res=>res.json())
+    .then(() => {
+      deleteToy(id)
+    })
+  }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{likes} Likes </p>
+      <button onClick={handleLike} className="like-btn">Like {"<3"}</button>
+      <button onClick={handleDelete} className="del-btn">Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({toys, updateToyArr, deleteToy}) {
+
+  const toyCards = toys.map(toy=>{
+    return <ToyCard key={toy.id} toy={toy} updateToyArr={updateToyArr} deleteToy={deleteToy}/>
+  })
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toyCards}</div>
   );
 }
 
